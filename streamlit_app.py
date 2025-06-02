566aafaace98271b1f4d8bd7cd3ffffaa3b5645f
fathurakbr <fathurakbarf4@gmail.com>
2025-06-02 19:24:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Belajar informatika asik")
 st.write(
     "Fathur Akbar Ismail Muhammad XB"
 )
-st.image("WhatsApp Image 2025-06-02 at 19.23.02_073772d0.jpg" width=200)
+st.image("WhatsApp Image 2025-06-02 at 19.23.02_073772d0.jpg", width=200)
